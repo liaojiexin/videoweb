daf73d288ee438c14c14dae5c4ca8b2b371b47fa
liaojiexin <570839281@qq.com>
2020-06-07 11:05:01 +0800

    commit

--- src/main/java/com/liaojiexin/videoweb/VideowebApplication.java
+++ src/main/java/com/liaojiexin/videoweb/VideowebApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
 //@MapperScan(value = "com.liaojiexin.videoweb.mapper") //该标签可以让指定包下的文件为Mapper，这样就该包下文件可以省略@Mapper标签
 @SpringBootApplication    //@SpringBootApplication来标注一个主程序类，说明这是一个spring boot应用
 //@ServletComponentScan     //新加@ServletComponentScan注解，并且继承SpringBootServletInitializer 部署成war包
+@EnableCaching      //开启基于注解的缓存
 public class VideowebApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/liaojiexin/videoweb/VideowebApplicationTests.java
+++ src/test/java/com/liaojiexin/videoweb/VideowebApplicationTests.java
@@ -1,19 +1,18 @@
 package com.liaojiexin.videoweb;
 
-import org.apache.catalina.core.ApplicationContext;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.*;
 
 //Spring Boot单元测试
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class VideowebApplicationTests {
+public class VideowebApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        System.out.println("test");
     }
 
 }
